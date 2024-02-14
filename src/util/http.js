fab88247fd67aa07b7da77cb84708fc9d35657ea
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-14 13:21:14 +0100

    updated http.js file

--- src/util/http.js
+++ src/util/http.js
@@ -1,5 +1,4 @@
-import { QueryClient } from "react-query";
-
+import { QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient();
 
@@ -60,4 +59,35 @@ export async function fetchSelectableImages({ signal }) {
   const { images } = await response.json();
 
   return images;
+}
+
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, { signal });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
 }
